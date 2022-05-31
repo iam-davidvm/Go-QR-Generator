12a429b762870b3bc97f47f2031c98f48d1e5426
iam-davidvm <vanmelkebekedavid@gmail.com>
2022-05-31 20:30:32 +0200

    Refactor openFile and generateURL

--- qr-generator.go
+++ qr-generator.go
@@ -27,21 +27,43 @@ func selectColumn(sheetValues [][]string) []string {
 	index := indexOfColumn(columns, strings.ToUpper(columnLetter))
 	//fmt.Println(index)
 
-	var hasHead string
-	fmt.Println("Heeft jouw kolom een titel? [y] [n]")
-	fmt.Scan(&hasHead)
+	// ask if there is a title column
+	var hasTitle string
+	for {
+		fmt.Println("Heeft jouw kolom een titel? [y] [n]")
+		fmt.Scan(&hasTitle)
+		hasTitle = strings.ToLower(hasTitle)
+		if hasTitle == "y" || hasTitle == "n" {
+			break
+		}
+	}
 
-	// TODO: put this in sperate function?
+	// 2 url options
+	var chooseOption string
+	for {
+		fmt.Println("Onmiddellijk naar betaalflow [1] of de flow doorlopen [2]")
+		fmt.Scan(&chooseOption)
+		if chooseOption == "1" {
+			chooseOption = "direct-payment"
+			break
+		} else if chooseOption == "2" {
+			chooseOption = "member-flow"
+			break
+		}
+	}
+
+	// TODO: put this in seperate function?
 	// TODO: regex check ondernemingsnummer format?
-	var ondernemingsnummers []string
+	var cellValues []string
 
 	for i, rowCell := range sheetValues[index] {
-		if i == 0 && strings.ToLower(hasHead) == "y" {
+		if i == 0 && hasTitle == "y" {
 			continue
 		}
-		ondernemingsnummers = append(ondernemingsnummers, rowCell)
+		rowCell = fmt.Sprintf("https://www.google.com/%v/%v", chooseOption, rowCell)
+		cellValues = append(cellValues, rowCell)
 	}
-	return ondernemingsnummers
+	return cellValues
 }
 
 func getValues(f *excelize.File, sheetName string) [][]string {
@@ -57,23 +79,12 @@ func getSheetName(f *excelize.File) string {
 	return f.GetSheetName(0)
 }
 
-func askFileName() string {
-	fmt.Println("Geef de naam en extensie van jouw bestand [lijst.xlsx]")
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan()
-	fileName := scanner.Text()
-	return fileName
-}
-
-func main() {
-	fileName := askFileName()
-	fmt.Println(fileName)
-
+func openSourceFile(fileName string) [][]string {
 	// Open File
 	f, err := excelize.OpenFile(fileName)
 	if err != nil {
 		fmt.Println("Oops, er ging iets verkeerd")
-		return
+		os.Exit(1)
 	}
 
 	// Close file when done
@@ -86,6 +97,23 @@ func main() {
 	// get all values of the sheet
 	sheetValues := getValues(f, sheetName)
 	//fmt.Println(sheetValues)
+
+	return sheetValues
+}
+
+func askFileName() string {
+	fmt.Println("Geef de naam en extensie van jouw bestand [lijst.xlsx]")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	fileName := scanner.Text()
+	return fileName
+}
+
+func main() {
+	fileName := askFileName()
+	fmt.Println(fileName)
+
+	sheetValues := openSourceFile(fileName)
 
 	// get the values of a specific column
 	columnValue := selectColumn(sheetValues)
